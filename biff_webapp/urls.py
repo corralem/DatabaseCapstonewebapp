d6b3878545414856bec37b348456a46248d31306
aziz16034 <67529117+aziz16034@users.noreply.github.com>
2021-04-21 03:49:25 -0500

    first commit

--- biff_webapp/urls.py
+++ biff_webapp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from website import urls, views
+from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('website/',include('website.urls')),
+    path('users/', user_views.register, name ='register'),
 ]
