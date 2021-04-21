e2e5181b14961bbe97891599c4f28daa4cd6f910
ChristianeUmutoni <79870149+ChristianeUmutoni@users.noreply.github.com>
2021-04-21 00:46:15 -0500

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('contact/', views.contact, name='contact'),
-     path('about/', views.about, name='about'),
+    path('about/', views.about, name='about'),
     
 ]
 
